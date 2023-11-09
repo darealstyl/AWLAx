6562ee1fc14a2e2347576b4b249fdbd71a642e81
Evan Greenstein <evanlg16@gmail.com>
2023-11-09 00:33:48 -0500

    Prevented axolotl from sticking to walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
@@ -9,7 +10,7 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
-
+    BoxCollider2D boxCollider;
     public float maxHealth;
     public float maxRunSpeed;
     public float jumpForce = 10.0f; // Set your desired jump force
@@ -26,12 +27,12 @@ public class PlayerController : MonoBehaviour
 
     float horizontal = 0.0f;
     float vertical = 0.0f;
+    bool canRun = true;
     bool dashInput = false;
     bool jumpInput = false; // Flag to check if jump was requested
     float dashElapsed;
 
     public LevelTimer levelTimer;
-    private bool hasDashed;
 
     // Start is called before the first frame update
     void Start()
@@ -39,6 +40,7 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
 
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
@@ -55,47 +57,24 @@ public class PlayerController : MonoBehaviour
             horizontal = Input.GetAxis("Horizontal");
             vertical = Input.GetAxis("Vertical");
 
-            //dashInput = Input.GetKeyDown(KeyCode.Return) ? !dashInput : dashInput;
-            //jumpInput = Input.GetKeyDown(KeyCode.Space) && isGrounded ? !jumpInput : jumpInput;
-
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 dashInput = true;
             }
-
 
             if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             {
                 jumpInput = true;
             }
 
-            
+
         }
     }
 
     void FixedUpdate()
     {
-        if (levelTimer.levelStarted)
-        {
-            // Check if the player is grounded
-            isGrounded = Physics2D.Raycast(rb.position, Vector2.down, groundCheckDistance, groundLayer).collider != null;
-            Debug.DrawRay(rb.position, Vector2.down * groundCheckDistance, Color.red);
-        }
-
-        if (isGrounded)
-        {
-            hasDashed = false;
-        }
-
-        // If a jump is requested and the player is grounded then add a vertical force
-        if (jumpInput && isGrounded)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            jumpInput = false; // Reset the jump input flag
-        }
-
         // Handle dashing
-        if (dashInput && dashElapsed >= dashDuration && !hasDashed)
+        if (dashInput && dashElapsed >= dashDuration)
         {
             Vector2 inputAxes = new Vector2(horizontal, vertical).normalized;
             rb.velocity += inputAxes * dashForce; // Apply the dash force in the input direction
@@ -103,19 +82,40 @@ public class PlayerController : MonoBehaviour
             TakeRecoilDamage();
             animator.SetBool("isSwimming", true);
             dashInput = false; // Reset the dash input flag immediately
-            hasDashed = true;
         }
+        // Dashing animation started
         else if (dashElapsed < dashDuration)
         {
             dashElapsed += Time.fixedDeltaTime;
         }
-        else
+        else if (dashElapsed >= dashDuration)
         {
-            // Ensure the dashElapsed timer does not exceed the dashDuration + threshold to avoid small deltaTime additions
-            dashElapsed = Mathf.Min(dashElapsed, dashDuration + 0.01f);
+            dashInput = false;
+
+            if (levelTimer.levelStarted)
+            {
+                // Check if the player is grounded
+                isGrounded = Physics2D.Raycast(rb.position, Vector2.down, groundCheckDistance, groundLayer).collider != null;
+                Debug.DrawRay(rb.position, Vector2.down * groundCheckDistance, Color.red);
+
+
+            }
+
+            // If a jump is requested and the player is grounded then add a vertical force
+            if (jumpInput && isGrounded)
+            {
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                jumpInput = false; // Reset the jump input flag
+            }
+
+            // float halfColliderWidth = boxCollider.size.x / 2.0f;
+            // bool isCollidingRight = Physics2D.Raycast(rb.position, Vector2.right, groundCheckDistance + halfColliderWidth, groundLayer).collider != null;
+            // bool isCollidingLeft = Physics2D.Raycast(rb.position, Vector2.left, groundCheckDistance + halfColliderWidth, groundLayer).collider != null;
+            // Debug.DrawRay(rb.position, Vector2.right * (groundCheckDistance + halfColliderWidth), Color.red);
+            // Debug.DrawRay(rb.position, Vector2.left * (groundCheckDistance + halfColliderWidth), Color.red);
 
             // Running logic
-            if (Mathf.Abs(horizontal) > 0.2f && currentRunSpeed > 0.0f)
+            if (Mathf.Abs(horizontal) > 0.2f && currentRunSpeed > 0.0f && canRun)
             {
                 float targetSpeed = horizontal * currentRunSpeed;
                 // Apply target speed but do not modify y velocity
@@ -127,18 +127,25 @@ public class PlayerController : MonoBehaviour
             {
                 animator.SetBool("isRunning", false);
             }
+
         }
 
-        if (dashInput && !isGrounded)
-        {
-            dashInput = false;
-        }
+
 
         // Reset swimming animation if dash is complete
         if (dashElapsed >= dashDuration && animator.GetBool("isSwimming"))
         {
             animator.SetBool("isSwimming", false);
         }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+            canRun = isGrounded;
+        }
+
     }
 
 
